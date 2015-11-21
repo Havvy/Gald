4f0aae2858ae40689b5bff28f87569a59f814d5f
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2015-11-21 09:44:51 +0000

    alias in Gald.Controller

--- apps/gald/lib/gald/controller.ex
+++ apps/gald/lib/gald/controller.ex
@@ -1,4 +1,3 @@
-# TODO(Havvy): Rename me to something that handles 
 defmodule Gald.Controller do
   @moduledoc """
   This module controls the supervisor and configuration of the race.
@@ -11,11 +10,17 @@ defmodule Gald.Controller do
   use GenServer
   use Gald.Race
   import ShortMaps
+  alias Gald.Race
+  alias Gald.Config
+  alias Gald.Players
+  alias Gald.Player
+  alias Gald.Snapshot
+  alias Gald.TurnOrder
 
   @type status :: :lobby | :play | :over
 
-  @spec start_link(Race.t, %Gald.Config{}) :: {:ok, pid}
-  @spec start_link(Race.t, %Gald.Config{}, GenServer.opts) :: {:ok, pid}
+  @spec start_link(Race.t, %Config{}) :: {:ok, pid}
+  @spec start_link(Race.t, %Config{}, GenServer.opts) :: {:ok, pid}
   def start_link(race, config, opts \\ []) do
     GenServer.start_link(__MODULE__, ~m{race config}a, opts)
   end
@@ -27,9 +32,9 @@ defmodule Gald.Controller do
 
   def handle_call({:new_player, name}, _from, state = ~m{race status}a) do
     reply = if status == :lobby do
-      case Gald.Players.new_player(players(race), name) do
+      case Players.new_player(players(race), name) do
         {:ok, player} ->
-          {:ok, Gald.Player.io(player)}
+          {:ok, Player.io(player)}
         {:error, reason} ->
           {:error, reason}
       end
@@ -41,7 +46,7 @@ defmodule Gald.Controller do
   end
 
   def handle_call(:snapshot, _from, state) do
-    {:reply, Gald.Snapshot.new(state), state}
+    {:reply, Snapshot.new(state), state}
   end
 
   def handle_call(:config, _from, state), do: {:reply, state.config, state}
@@ -49,42 +54,42 @@ defmodule Gald.Controller do
   def handle_cast(:begin, state = ~m{race config}a) do
     state = %{state | status: :play}
     # Determine turn order
-    turn_order = Gald.TurnOrder.calculate(players(race))
+    turn_order = TurnOrder.calculate(players(race))
 
     victory_config = %{
       end_space: config.end_space,
       race: race
     }
-    {:ok, _victory} = Gald.Race.start_victory(race, victory_config)
+    {:ok, _victory} = Race.start_victory(race, victory_config)
 
     # Start the Map
     map_config = %{
-      players: Gald.Players.names(players(race)),
+      players: Players.names(players(race)),
       end_space: state.config.end_space
     }
-    {:ok, _map} = Gald.Race.start_map(race, map_config)
+    {:ok, _map} = Race.start_map(race, map_config)
 
     # Start the screen monitoring processing.
-    {:ok, _display} = Gald.Race.start_display(race, %{})
+    {:ok, _display} = Race.start_display(race, %{})
 
     # Start the event choosing process.
     event_manager_config = ~m{config}a
-    {:ok, _event_manager} = Gald.Race.start_event_manager(race, event_manager_config)
+    {:ok, _event_manager} = Race.start_event_manager(race, event_manager_config)
 
-    snapshot = Gald.Snapshot.new(%{state | status: :beginning})
+    snapshot = Snapshot.new(%{state | status: :beginning})
     GenEvent.notify(out(race), {:begin, snapshot.data})
 
     round_config = %{
       turn_order: turn_order
     }
-    {:ok, _round} = Gald.Race.start_round(race, round_config)
+    {:ok, _round} = Race.start_round(race, round_config)
 
     {:noreply, state}
   end
 
   def handle_cast(:finish, state = ~m{race}a) do
     state = %{state | status: :over}
-    snapshot = Gald.Snapshot.new(state)
+    snapshot = Snapshot.new(state)
     GenEvent.notify(out(race), {:finish, snapshot.data})
     {:noreply, state}
   end
