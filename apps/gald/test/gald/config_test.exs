154f0988d199ed35cf828065af65043d2f7eb30b
Ryan Scheel (Havvy) <ryan.havvy@gmail.com>
2015-11-22 23:22:35 +0000

    Remove some useless tests.

--- apps/gald/test/gald/config_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Gald.RaceConfigTest do
-  use ExUnit.Case, async: true
-
-  test "Default value for end space." do
-    {:ok, race} = Gald.Race.start_link(%Gald.Config{})
-    assert 120 = Gald.Race.config(race).end_space
-  end
-end
